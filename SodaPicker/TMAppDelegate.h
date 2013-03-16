3305e4e9cf3d50cd2cff7447047388aae39b806c
John Tumminaro <jtumminaro@thought-matrix.com>
2013-03-16 15:10:01 -0700

    Initial Commit

---
+++ SodaPicker/TMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TMAppDelegate.h
+//  SodaPicker
+//
+//  Created by John Tumminaro on 3/16/13.
+//  Copyright (c) 2013 ThoughtMatrix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SodaPicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SodaPicker
+//
+//  Created by John Tumminaro on 3/16/13.
+//  Copyright (c) 2013 ThoughtMatrix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMAppDelegate class]));
+    }
+}
